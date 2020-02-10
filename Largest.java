3ab48943220fba4b1f6241be4e4382b868fd34bc
iramulmurshed <35691070+iramulmurshed@users.noreply.github.com>
2020-02-10 15:31:18 +0600

    Add files via upload

---
+++ Largest.java
@@ -0,0 +1,33 @@
+
+package largest;
+
+import java.util.Scanner;
+
+
+public class Largest {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         {
+    int x, y, z;
+    System.out.println("Enter three integers");
+    Scanner in = new Scanner(System.in);
+
+    x = in.nextInt();
+    y = in.nextInt();
+    z = in.nextInt();
+
+    if (x > y && x > z)
+      System.out.println("First number is the largest.");
+    else if (y > x && y > z)
+      System.out.println("Second number is the largest.");
+    else if (z > x && z > y)
+      System.out.println("Third number is the largest.");
+    else
+      System.out.println("The numbers are not distinct.");
+  }
+    }
+    
+}
